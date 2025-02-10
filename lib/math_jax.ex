fa2808e9c4924a6119a84336f00ac51c85c689d8
Alex Koutmos <akoutmos@gmail.com>
2025-02-09 21:44:35 -0500

    Initial commit

---
+++ lib/math_jax.ex
@@ -0,0 +1,18 @@
+defmodule MathJax do
+  @moduledoc """
+  Documentation for `MathJax`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MathJax.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/math_jax_test.exs
@@ -0,0 +1,8 @@
+defmodule MathJaxTest do
+  use ExUnit.Case
+  doctest MathJax
+
+  test "greets the world" do
+    assert MathJax.hello() == :world
+  end
+end
