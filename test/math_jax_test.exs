95e9f53ecd63f3e877c442008caee4e22c4161cd
Alex Koutmos <akoutmos@gmail.com>
2026-03-19 22:33:31 -0500

    Trying to fix CI

--- test/math_jax_test.exs
+++ test/math_jax_test.exs
@@ -1,3 +1,23 @@
 defmodule MathJaxTest do
   use ExUnit.Case
+
+  describe "The render/2 function" do
+    test "should render a valid mathematical expression as an SVG" do
+      assert is_binary(MathJax.render("SMA = {P_1 + P_2 + P_3 + {..}. + P_n \over n}", :svg))
+    end
+
+    test "should render a valid mathematical expression as an PNG" do
+      assert is_binary(MathJax.render("SMA = {P_1 + P_2 + P_3 + {..}. + P_n \over n}", :png))
+    end
+  end
+
+  describe "The render_inline/2 function" do
+    test "should render a valid mathematical expression as an SVG" do
+      assert is_binary(MathJax.render_inline("SMA = {P_1 + P_2 + P_3 + {..}. + P_n \over n}", :svg))
+    end
+
+    test "should render a valid mathematical expression as an PNG" do
+      assert is_binary(MathJax.render_inline("SMA = {P_1 + P_2 + P_3 + {..}. + P_n \over n}", :png))
+    end
+  end
 end
